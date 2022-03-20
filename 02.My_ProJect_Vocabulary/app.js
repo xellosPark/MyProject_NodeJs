91f7b63ca823a55b9650bac36362c96e9cc7d7c4
yuki <yukihiro@ubisam.com>
2022-03-20 23:13:15 +0900

    server complete

--- 02.My_ProJect_Vocabulary/app.js
+++ 02.My_ProJect_Vocabulary/app.js
@@ -33,18 +33,34 @@
 //fs.writeFileSync("./test.json", JSON.stringify(ob, null, 2));
 
 // HTTP 서버 만들기
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer( (req,res) => {
-    if(req.url === "/"){
-        res.write("<h1>Hello from nodejs</h1>");    
-    }else{
-        res.write(`<h1>You have entered : ${req.url} </h1>`);
-    }
-    res.end();
+// const server = http.createServer( (req,res) => {
+//     if(req.url === "/"){
+//         res.write("<h1>Hello from nodejs</h1>");    
+//     }else{
+//         res.write(`<h1>You have entered : ${req.url} </h1>`);
+//     }
+//     res.end();
+// });
+
+// server.listen(3000, ()=>{
+//     console.log(" The server is listening on port 3000");
+// });
+
+// EXPRESS 이용하여 HTTP 서버 만들기
+const express = require('express');
+const server = express();
+
+server.get("/",(req,res) => {
+    res.send("<h1>Hello from nodejs</h1>");
 });
+//GET       www.facebook.com/
+//PORT      ID:abc Pass:1234
+//DELETE    
+//PUT       
 
-server.listen(3000, ()=>{
-    console.log(" The server is listening on port 3000");
+server.listen(3000,(err) => {
+    if(err) return console.log("err log :" + err);
+    console.log("The server if listening on port 3000");
 });
-
