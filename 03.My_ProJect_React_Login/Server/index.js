4dc9eef19bf23d39d21424f5aae9e8ec42900923
yuki <yukihiro@ubisam.com>
2022-03-26 21:46:49 +0900

    cofig file add

--- 03.My_ProJect_React_Login/Server/index.js
+++ 03.My_ProJect_React_Login/Server/index.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+// Db Schema 데이를 갖져 온다
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
 //MongoDB 연결
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://xellos:sv7788@boilerplate.g1rjs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   //6.0 이상에서 자동으로 설정
@@ -14,7 +27,22 @@ mongoose.connect('mongodb+srv://xellos:sv7788@boilerplate.g1rjs.mongodb.net/myFi
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!~~~안녕하세요 ~')
+})
+
+app.post('/register', (req, res)=>{
+  // 회원 가입 할때 필요한 정보들을 Client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+  // MongoDb 
+  user.save((err, doc) => {
+    // console.log("메세지 확인");
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
